cdc22197f516f1de938525147d334624f364bac7
akashrpo <akashguptarpo@gmail.com>
2018-11-07 16:51:59 +0530

    Checks valid/Invalid Credentials while login/singup

--- app/src/main/java/com/example/akash/marksuploader/Login.java
+++ app/src/main/java/com/example/akash/marksuploader/Login.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class Login extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +33,6 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        Button btn2 = (Button) findViewById(R.id.button_l);
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login.this, student.class));
-            }
-        });
 
         Button btn3 = (Button) findViewById(R.id.button007);
 
@@ -40,5 +43,26 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        Button btn2 = (Button) findViewById(R.id.button_l);
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEmptyField((EditText) findViewById(R.id.editText4))) {
+                    Toast.makeText(getApplicationContext(), "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+                }
+                else if(isEmptyField((EditText) findViewById(R.id.editText2))) {
+                    Toast.makeText(getApplicationContext(), "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+                } else {
+                    startActivity(new Intent(Login.this, student.class));
+                }
+            }
+        });
+
+    }
+
+    boolean isEmptyField(EditText editText) {
+        return editText.getText().toString().length() <= 0;
     }
 }
--- app/src/main/java/com/example/akash/marksuploader/singup.java
+++ app/src/main/java/com/example/akash/marksuploader/singup.java
@@ -8,16 +8,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class singup extends AppCompatActivity {
 
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
 
         TextView btn = (TextView) findViewById(R.id.link_login);
 
@@ -28,7 +38,7 @@ public class singup extends AppCompatActivity {
             }
         });
 
-        Spinner myspin = (Spinner) findViewById(R.id.spinner);
+        final Spinner myspin = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<String> myadapter = new ArrayAdapter<String>(singup.this,
                 R.layout.activity_spin_item,
@@ -36,5 +46,55 @@ public class singup extends AppCompatActivity {
 
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myspin.setAdapter(myadapter);
+
+        Button btn2 = (Button) findViewById(R.id.btn_signup);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEmptyField((EditText) findViewById(R.id.editText7))) { //Reg No.
+                    Toast.makeText(getApplicationContext(), "Fill all fields!!", Toast.LENGTH_SHORT).show();
+                } else if (isEmptyField((EditText) findViewById(R.id.editText8))) { //Password
+                    Toast.makeText(getApplicationContext(), "Fill all fields!!", Toast.LENGTH_SHORT).show();
+                } else if (isEmptyField((EditText) findViewById(R.id.editText))) { //Phone
+                    Toast.makeText(getApplicationContext(), "Fill all fields!!", Toast.LENGTH_SHORT).show();
+                } else if (isEmptyField((EditText) findViewById(R.id.editText5))) { //Name
+                    Toast.makeText(getApplicationContext(), "Fill all fields!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (isSpin(myspin).equals("Select Department")) {
+                        Toast.makeText(getApplicationContext(), "Select Your Department.", Toast.LENGTH_SHORT).show();
+                    } else if (isEmptyFieldph((EditText) findViewById(R.id.editText))) { //Phone
+                        Toast.makeText(getApplicationContext(), "Invalid Phone Number!", Toast.LENGTH_SHORT).show();
+                    } else if (isEmptyFieldpass((EditText) findViewById(R.id.editText8))) { //Password
+                        Toast.makeText(getApplicationContext(), "Password atleast 8 char long", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Hooray!\nRegistration Succcessful!!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), Login.class));
+                    }
+                }
+            }
+        });
+
+    }
+
+    String isSpin(Spinner spinner) {
+        String Name = spinner.getSelectedItem().toString();
+        return Name;
+    }
+
+    boolean isEmptyField(EditText editText) {
+        return editText.getText().toString().length() <= 0;
+    }
+
+    boolean isEmptyFieldpass(EditText editText) {
+        return editText.getText().toString().length() <= 7;
+    }
+
+    boolean isEmptyFieldph(EditText editText) {
+        boolean flag = true;
+        if (editText.getText().toString().length() == 10) {
+            flag = false;
+        }
+        return flag;
     }
 }
