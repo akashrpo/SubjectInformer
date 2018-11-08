fb2eed190ddcd240e2cf8bb9819312a9f9491ca2
akashrpo <akashguptarpo@gmail.com>
2018-11-08 15:00:43 +0530

    make new class

---
+++ app/src/main/java/com/example/akash/marksuploader/testtttttttttttttttt.java
@@ -0,0 +1,15 @@
+package com.example.akash.marksuploader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class testtttttttttttttttt {
+
+
+
+    protected void onCreathe() {}
+
+}
